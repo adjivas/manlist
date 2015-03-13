591e491b58f2441ad00ee15e9ccf4be26bea7ad6
adjivas <adjivas@users.noreply.github.com>
2015-03-14 00:26:21 +0100

    add env and read first line

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,55 @@
 #![feature(fs, io, path, collections)]
+use std::os;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::path::AsPath;
+use std::borrow::ToOwned;
 
-fn list(roots: &Vec<String>) {
+fn list(roots: &Vec<String>) -> Vec<File> {
   let mut files: Vec<File> = Vec::with_capacity(roots.capacity());
   for path in roots {
-
     match std::fs::walk_dir(&Path::new(path)) {
-      Err(why) => println!("!walk_dir {:?}", why.kind()),
+      Err(why) => println!("walk_dir {:?}", why.kind()),
       Ok(paths) => {
         for path in paths {
           let buf = path.unwrap().path();
-
-          if buf.file_name().is_some() {
-            //if buf.extension() == "1" {
-              println!("{:?}", buf);
-              println!("{:?}", buf.file_name());
-            //}
+          if buf.extension().is_some()
+          && buf.extension().unwrap() == "1" {
+            match File::open(&buf) {
+              Err(why) => println!("Could not open {:?}: {}", buf.file_name().unwrap(), why.description()),
+              Ok(file) => {
+                files.push(file)
+              },
+            }           
           }
         }
       },
     }
   }
+  files
 }
 
 fn main() {
-  let mut roots = Vec::new();
+  let key = "MANPATH";
 
-  roots.push("/nfs/zfs-student-5/users/2013/adjivas/.brew/share/man/man1/aclocal-1.15.1".to_string());
+  match os::getenv(key) {
+    None => println!("the ${:?} from environement is empty! ", key),
+    Some(manpath) => {
+      let roots: Vec<String> = manpath.split_str(":").map(|x| x.to_string()).collect();
 
-  roots.push("/nfs/zfs-student-5/users/2013/adjivas/.brew/share/man".to_string());
-  roots.push("/usr/local/share/man".to_string());
-  roots.push("/usr/share/man".to_string());
-  roots.push("/nfs/zfs-student-5/users/2013/adjivas/.brew/Cellar/rust/1.0.0-alpha.2/share/man".to_string());
-  roots.push("/nfs/zfs-student-5/users/2013/adjivas/usr/share/man".to_string());
-  roots.push("/Applications/Xcode.app/Contents/Developer/usr/share/man".to_string());
-  roots.push("/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/folders.push(share/man".to_string());
+      let files: Vec<File> = list(&roots);
 
-  list(&roots);
+      for file in files {
+        let mut content = BufReader::new(file);
+        let mut line = String::new();
 
+        while content.read_line(&mut line).is_ok() && !line.is_empty() {
+          println!("{:?}", line);
+          break ;
+          line.clear();
+        }
+      }
+    },
+  }
 }
